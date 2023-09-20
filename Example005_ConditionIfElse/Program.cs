002f377c6c6cdb600b50a2a2afb811e8d2852b1e
Dima <perebyakin_dmitr@mail.ru>
2023-09-20 13:25:30 +0300

    Add Example005_ConditionIfElse

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username == "Маша")
+if(username.ToLower() == "маша")
 {
-    Console.WriteLine("Ура, это же МАША");
+    Console.WriteLine("Ура, это же МАША!");
 }
 else
 {
-    Console.WriteLine("Привет")
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
 }
